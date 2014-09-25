476ecee338994788aaffc1827fdc93ba828fbc86
Beth Reid <beth.ann.reid@gmail.com>
2014-09-24 17:26:58 -0700

    got python to C communication working correctly in test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ctypes import Structure, POINTER, c_int, c_double, c_void_p, CDLL
+import sys
 
 # Here's our C structure defined in Python
 class xibininfo(Structure):
@@ -12,12 +13,43 @@ class dset(Structure):
              ("vx",c_void_p), ("vy",c_void_p), ("vz",c_void_p),\
              ("wgt",c_void_p), ("np",c_int)
 
-test = CDLL("test.so")
+## works on mac.
+#test = CDLL("test.so")
+## need full path on linux for some reason!
+test = CDLL("/home/howdiedoo/gitprojects/xi/test.so")
+
+testfname = "/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short"
+# on desktop
+testfname = "/home/howdiedoo/SOmaster/PB00_0.6452.halos.short"
 
 ## read in numpy array.
 ### ack these give me things that are offset.
-px,py,pz,w1 = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[0,1,2,3],unpack=True)
-vx,vy,vz = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[0,1,2],unpack=True)
+px,py,pz,w1 = np.loadtxt(testfname,usecols=[0,1,2,3],unpack=True)
+vx,vy,vz = np.loadtxt(testfname,usecols=[0,1,2],unpack=True)
+
+#px = px.copy(order='C') ## 'C' is the default, means put it in memory continuously.
+## can check the result by looking at the "stride"
+#aa = np.arange(0,16,1).reshape(2,8)
+#b=aa[:,0]
+#print b.strides
+#b=aa[:,0].copy()
+#print b.strides
+## returns
+#(64,)
+#(8,)
+# (number of bytes to skip to to get the next element.
+## this didn't work.
+#for tmp in [px,py,pz,vx,vy,vz,w1]:
+#  tmp = tmp.copy(order='C')
+
+px = px.copy(order='C')
+py = py.copy(order='C')
+pz = pz.copy(order='C')
+vx = vx.copy(order='C')
+vy = vy.copy(order='C')
+vz = vz.copy(order='C')
+w1 = w1.copy(order='C')
+
 
 print 'well this?'
 test.testsingle(px.ctypes.data_as(c_void_p),len(px))
@@ -75,14 +107,15 @@ test.hellop(d1)
 # native outputs from np.loadtxt are screwed up!
 test.hellop(d2)
 print 'that is broken, what if we read one column at a time?'
+print 'nevermind, fixed after contiguous memory copy.'
 
-px = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[0])
-py = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[1])
-pz = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[2])
-vx = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[0])
-vy = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[1])
-vz = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[2])
-www = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short",usecols=[3])
+px = np.loadtxt(testfname,usecols=[0])
+py = np.loadtxt(testfname,usecols=[1])
+pz = np.loadtxt(testfname,usecols=[2])
+vx = np.loadtxt(testfname,usecols=[0])
+vy = np.loadtxt(testfname,usecols=[1])
+vz = np.loadtxt(testfname,usecols=[2])
+www = np.loadtxt(testfname,usecols=[3])
 
 print 'does this work better? yes, this seems to work.  now try single numpy array?  I think we need to worry about the memory being contiguous or something.'
 d3 = dset(px.ctypes.data_as(c_void_p),py.ctypes.data_as(c_void_p),pz.ctypes.data_as(c_void_p),\
@@ -90,11 +123,14 @@ d3 = dset(px.ctypes.data_as(c_void_p),py.ctypes.data_as(c_void_p),pz.ctypes.data
           www.ctypes.data_as(c_void_p),len(px))
 test.hellop(d3)
 
-aa = np.loadtxt("/Users/bareid/gitprojects/BIDS/xi/usecase/PB00_0.6452.halos.short")
+
+print 'Yu says this shouldnt work, or at least is sketchy in terms of memory freeing/tracking.  so we will do the trick of copying the arrays into contiguous points in memory by hand.'
+aa = np.loadtxt(testfname)
 print aa.shape
 
 d4 = dset((aa[:,0]).ctypes.data_as(c_void_p), (aa[:,1]).ctypes.data_as(c_void_p), (aa[:,2]).ctypes.data_as(c_void_p),\
           (aa[:,0]).ctypes.data_as(c_void_p), (aa[:,1]).ctypes.data_as(c_void_p), (aa[:,2]).ctypes.data_as(c_void_p),\
           (aa[:,3]).ctypes.data_as(c_void_p))
 test.hellop(d4)
+
 
